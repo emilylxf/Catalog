11d3db5136c1699dc46901ea1e5fa1839561610d
YiFei Li <yifeil@berkeley.edu>
2021-06-27 09:57:18 -0700

    refactor: update port settings

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const ISSUER = "https://dev-19217834.okta.com/oauth2/default"
 const config = {
   clientId: CLIENT_ID,
   issuer: ISSUER,
-  redirectUri: 'http://localhost:8080/login/callback',
+  redirectUri: 'http://localhost:3000/login/callback',
   scopes: ['openid', 'profile', 'email'],
   pkce: true
 };
