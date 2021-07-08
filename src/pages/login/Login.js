f4e9618091fdee4edcd0607ce03729ecae25a392
YiFei <yifeil@berkeley.edu>
2021-07-08 14:53:40 -0700

    feat: add submit function for story content

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import { Redirect } from 'react-router-dom';
 import OktaSignInWidget from './OktaSignInWidget';
 import { useOktaAuth } from '@okta/okta-react';
@@ -9,6 +10,22 @@ const Login = ({ config }) => {
   const { oktaAuth, authState } = useOktaAuth();
 
   const onSuccess = (tokens) => {
+    //use localhost:5000 for dev purpose
+    axios
+      .get("http://127.0.0.1:5000/user", { 
+        headers: {"Authorization" : `Bearer ${tokens.idToken.idToken}`} 
+      })
+      .then(
+        function (response) {
+          //Perform action based on response
+
+          localStorage.setItem("user_id", response.data)
+        }.bind(this)
+      )
+      .catch(function (error) {
+        console.log(error);
+        //Perform action based on error
+      });
     oktaAuth.handleLoginRedirect(tokens);
   };
 
--- src/pages/story/StoryWriter.js
+++ src/pages/story/StoryWriter.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 
 import "../../assets/css/StoryWriter.css";
+import { useOktaAuth } from '@okta/okta-react';
 import { Form, Container, Row, Col} from 'react-bootstrap';
 import { Remarkable } from 'remarkable';
 import MarkdownIt from 'markdown-it';
@@ -20,122 +22,150 @@ MdEditor.use(Plugins.TabInsert, {
 const mdParser = new MarkdownIt({
 });
 
-class StoryWriter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      view: {
-        menu: true,
-        md: true,
-        html: false
-      },
-      showMd: true,
-      content_html: '',
-      content_text: ''
+const StoryWriter = () => {
+  const { oktaAuth, authState } = useOktaAuth();
+  const [view, setView] = useState({
+    menu: true,
+    md: true,
+    html: false
+  });
+  const [showMd, setShowMd] = useState(true);
+  const [contentHtml, setContentHtml] = useState('');
+  const [contentText, setContentText] = useState('');
+  const [title, setTitle] = useState('');
+  const [transferFrom, setTransferFrom] = useState('');
+  const [transferTo, setTransferTo] = useState('');
+  const md = new Remarkable('full', {
+    html:         true,        // Enable HTML tags in source
+    xhtmlOut:     false,        // Use '/' to close single tags (<br />)
+    breaks:       true,        // Convert '\n' in paragraphs into <br>
+    langPrefix:   'language-',  // CSS language prefix for fenced blocks
+    linkify:      true,         // autoconvert URL-like texts to links
+    linkTarget:   '',           // set target to open link in
+  
+    // Enable some language-neutral replacements + quotes beautification
+    typographer:  false,
+  
+    // Double + single quotes replacement pairs, when typographer enabled,
+    // and smartquotes on. Set doubles to '«»' for Russian, '„“' for German.
+    quotes: '“”‘’',
+  });
+
+  function handleChangeView() {
+    setShowMd(!showMd)
+  }
+
+  function handleInputChange() {
+    
+  }
+
+  function handleEditorChange({ html, text }) {
+    setContentHtml(html)
+    setContentText(text)
+  }
+
+  function handleArticleSubmit() {
+    const config = {
+      headers: { Authorization: `Bearer ${authState.idToken.idToken}` }
+    };
+    if (!localStorage.getItem("user_id")) {
+      axios
+      .get("http://127.0.0.1:5000/user", config)
+      .then(
+        function (response) {
+          //Perform action based on response
+          localStorage.setItem("user_id", response.data)
+        }.bind(this)
+      )
+      .catch(function (error) {
+        console.log(error);
+        //Perform action based on error
+      });
     }
-    this.markdownClass = 'writer-panel'
-    this.handleChangeView = this.handleChangeView.bind(this);
-    this.handleEditorChange = this.handleEditorChange.bind(this);
-    this.hljs = require('highlight.js') // https://highlightjs.org/
-    this.md = new Remarkable('full', {
-      html:         true,        // Enable HTML tags in source
-      xhtmlOut:     false,        // Use '/' to close single tags (<br />)
-      breaks:       true,        // Convert '\n' in paragraphs into <br>
-      langPrefix:   'language-',  // CSS language prefix for fenced blocks
-      linkify:      true,         // autoconvert URL-like texts to links
-      linkTarget:   '',           // set target to open link in
-    
-      // Enable some language-neutral replacements + quotes beautification
-      typographer:  false,
-    
-      // Double + single quotes replacement pairs, when typographer enabled,
-      // and smartquotes on. Set doubles to '«»' for Russian, '„“' for German.
-      quotes: '“”‘’',
-    });
-  }
-
-  handleChangeView() {
-    this.setState({
-      showMd: !this.state.showMd
-    })
-  }
-
-  handleEditorChange({ html, text }) {
-    this.setState({
-      content_html: html,
-      content_text: text
-    })
+    const body = {
+      article_content: contentText,
+      article_title: "test_article"
+    }
+    axios
+      .post("http://127.0.0.1:5000/story?user_id="+localStorage.getItem("user_id"),
+      body,
+      config
+      ).then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
   
   
-  render() {
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          <button onClick={this.handleChangeView}>Toggle</button>
-        </div>
-        <Form>
-          <Container>
-            <Row className="modal_item">
-              <Col>
-                <Form.Label>Title</Form.Label>
-                <Form.Control
-                  name="title"
-                  type="text"
-                  value={this.state.title}
-                  placeholder="Title"
-                  onChange={this.handleInputChange}
-                ></Form.Control>
-              </Col>
-            </Row>
-          </Container>
-          <Container>
-            <Row className="modal_item">
-              <Col>
-                <Form.Label>Transfer From</Form.Label>
-                <Form.Control
-                  name="transfer_from"
-                  type="text"
-                  value={this.state.transfer_from}
-                  placeholder="N/A"
-                  onChange={this.handleInputChange}
-                ></Form.Control>
-              </Col>
-              <Col>
-                <Form.Label>Transfer To</Form.Label>
-                <Form.Control
-                  name="transfer_to"
-                  type="text"
-                  value={this.state.transfer_to}
-                  placeholder="N/A"
-                  onChange={this.handleInputChange}
-                ></Form.Control>
-              </Col>
-            </Row>
-          </Container>
-          <Form.Group className="form_group modal_item">
-            <Form.Label>Message</Form.Label>
-            {this.state.showMd && (
-              <MdEditor 
-              style = {{ marginLeft: '5rem', marginRight: '5rem' }}
-              value = {this.state.content_text}
-              markdownClass={this.markdownClass} 
-              view={this.state.view} 
-              renderHTML={text => mdParser.render(text)} 
-              onChange={this.handleEditorChange} 
-              />
-            )}
-            {!this.state.showMd && (
-              <div dangerouslySetInnerHTML={{__html: this.md.render(this.state.content_text)}} />
-              // <ReactMarkdown remarkPlugins={[this.gfm]}>{this.state.content_text}</ReactMarkdown>
-            )}
-            
-          </Form.Group>
-        </Form>
+        <button onClick={handleChangeView}>Toggle</button>
       </div>
-    );
-  }
+      <div>
+        <button onClick={handleArticleSubmit}>Submit</button>
+      </div>
+      <Form>
+        <Container>
+          <Row className="modal_item">
+            <Col>
+              <Form.Label>Title</Form.Label>
+              <Form.Control
+                name="title"
+                type="text"
+                value={title}
+                placeholder="Title"
+                onChange={handleInputChange}
+              ></Form.Control>
+            </Col>
+          </Row>
+        </Container>
+        <Container>
+          <Row className="modal_item">
+            <Col>
+              <Form.Label>Transfer From</Form.Label>
+              <Form.Control
+                name="transferFrom"
+                type="text"
+                value={transferFrom}
+                placeholder="N/A"
+                onChange={handleInputChange}
+              ></Form.Control>
+            </Col>
+            <Col>
+              <Form.Label>Transfer To</Form.Label>
+              <Form.Control
+                name="transferTo"
+                type="text"
+                value={transferTo}
+                placeholder="N/A"
+                onChange={handleInputChange}
+              ></Form.Control>
+            </Col>
+          </Row>
+        </Container>
+        <Form.Group className="form_group modal_item">
+          <Form.Label>Message</Form.Label>
+          {showMd && (
+            <MdEditor 
+            style = {{ marginLeft: '5rem', marginRight: '5rem' }}
+            value = {contentText}
+            view={view} 
+            renderHTML={text => mdParser.render(text)} 
+            onChange={handleEditorChange} 
+            />
+          )}
+          {!showMd && (
+            <div dangerouslySetInnerHTML={{__html: md.render(contentText)}} />
+          )}
+          
+        </Form.Group>
+      </Form>
+    </div>
+  );
 }
 
 export default StoryWriter;
